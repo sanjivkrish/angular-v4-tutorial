e14395fcbff6a52d461d2977374eb002388cd5d3
Sanjiv Krish <sanjiv436.k@gmail.com>
2017-06-12 00:13:23 +0530

    app: directive: bold: Implement directive
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

--- src/app/directives/bold/bold.directive.ts
+++ src/app/directives/bold/bold.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appBold]'
 })
 export class BoldDirective {
 
-  constructor() { }
+  constructor(private _el:ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter () {
+    this._el.nativeElement.style.fontWeight = 'bold';
+  }
+
+  @HostListener('mouseleave') onMouseLeave () {
+    this._el.nativeElement.style.fontWeight = null;
+  }
 
 }
