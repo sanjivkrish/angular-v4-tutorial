a060d574b16483b7338668f66657705670525608
Sanjiv Krish <sanjiv436.k@gmail.com>
2017-06-11 23:33:58 +0530

    app: list: Use 'movielist' service in the component
    Show movies in the displayed item
    
    Signed-off-by: Sanjiv Krish <sanjiv436.k@gmail.com>

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MovielistService } from '../services/movielist/movielist.service';
 
 @Component({
   selector: 'app-list',
@@ -9,7 +10,9 @@ export class ListComponent implements OnInit {
   listTitle: string = 'Sample list';
   listItems: any[] = ['one', 'two', 'three'];
 
-  constructor() { }
+  constructor(private _moviesList: MovielistService) {
+    this.listItems = this._moviesList.getMovies();
+  }
 
   ngOnInit() {
   }
